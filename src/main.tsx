32444f5cd799b89aa10a667167c74b3a32685d12
Ancient <gamesworldcraft@gmail.com>
2024-12-17 23:21:28 +0800

    fix:theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "@/i18n/i18n"; // 引入 i18n 配置文件
 import "./index.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,20 +1,33 @@
 import { createTheme } from "@mui/material/styles";
 import { enUS, zhCN } from "@mui/material/locale";
+import type { Localization } from "@mui/material/locale";
 
 // 动态生成主题
-export const getTheme = (locale: any) =>
+export const getTheme = (locale: Localization) =>
   createTheme(
     {
-      palette: {
-        primary: {
-          main: "#1976d2",
-        },
-        secondary: {
-          main: "#dc004e",
-        },
+      colorSchemes: { light: true, dark: true },
+      cssVariables: {
+        colorSchemeSelector: "class",
       },
       typography: {
         fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+      },
+      components: {
+        MuiAlert: {
+          styleOverrides: {
+            root: {
+              variants: [
+                {
+                  props: { severity: "info" },
+                  style: {
+                    backgroundColor: "#60a5fa",
+                  },
+                },
+              ],
+            },
+          },
+        },
       },
     },
     locale // 动态语言配置
