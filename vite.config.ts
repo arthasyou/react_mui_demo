5ec4d77a38ece0176cc3386b8e530e2904d88a29
Ancient <gamesworldcraft@gmail.com>
2024-12-20 15:55:13 +0800

    fix url

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(() => {
     server: {
       proxy: {
         "/api": {
-          target: "https://sx.valleyexc.cc",
+          target: "your-http-address",
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ""),
           ws: true,
